78d6796dbdc00303447b33f42278905f77107de5
Karl Broman <kbroman@gmail.com>
2019-05-28 20:08:27 -0500

    First working version

--- R/browse.R
+++ R/browse.R
@@ -5,6 +5,8 @@
 #' @param scan1output Genome scan output, as from [qtl2::scan1()]
 #' @param map Corresponding physical map, as a list of chromosomes
 #'     that are each a vector of marker positions.
+#' @param lodcolumn LOD score column to plot (a numeric index, or a
+#' character string for a column name). Only one value allowed.
 #' @param dir Optional directory to contain the results. If not
 #'     provided, a temporary directory is created.
 #'
@@ -12,20 +14,28 @@
 #'
 #' @export
 #' @importFrom utils unzip
+#' @importFrom qtl2 align_scan1_map
+#' @importFrom jsonlite toJSON
 #'
 #' @examples
 #' # run qtl2
 #' # browse results
 browse <-
-    function(scan1output, map, dir=NULL)
+    function(scan1output, map, lodcolumn=1, dir=NULL)
 {
+    if(is.null(map)) stop("map is NULL")
+    if(!is.list(map)) map <- list(" "=map) # if a vector, treat it as a list with no names
+
+    # align scan1 output and map
+    tmp <- qtl2::align_scan1_map(scan1output, map)
+    scan1output <- tmp$scan1
+    map <- tmp$map
+
     # make directory
     if(is.null(dir)) {
-        while(dir.exists(dir <-
-              file.path(tempdir(),
-                        paste0("qtl2_",
-                               paste0(sample(letters, 6, repl=TRUE),
-                                      collapse=""))))) {}
+        # create a randomized directory name
+        while(dir.exists(dir <- file.path(tempdir(),
+             paste0("qtl2_", paste0(sample(letters, 6, repl=TRUE), collapse=""))))) {}
     }
     if(!dir.exists(dir)) {
         dir.create(dir)
@@ -34,8 +44,36 @@ browse <-
     unzip(system.file("extdata", "ggb.zip", package="qtl2browse"),
           exdir=dir, overwrite=TRUE)
 
+    # grab lod scores
+    if(length(lodcolumn)==0) stop("lodcolumn has length 0")
+    if(length(lodcolumn) > 1) { # If length > 1, take first value
+        warning("lodcolumn should have length 1; only first element used.")
+        lodcolumn <- lodcolumn[1]
+    }
+    if(is.character(lodcolumn)) { # turn column name into integer
+        tmp <- match(lodcolumn, colnames(scan1output))
+        if(is.na(tmp)) stop('lodcolumn "', lodcolumn, '" not found')
+        lodcolumn <- tmp
+    }
+    if(lodcolumn < 1 || lodcolumn > ncol(scan1output))
+        stop("lodcolumn [", lodcolumn, "] out of range (should be in 1, ..., ",
+             ncol(scan1output), ")")
+    lod <- unclass(scan1output)[,lodcolumn]
+
+    # combine qtl2 data into data frame
+    result <- data.frame(chr=rep(names(map), vapply(map, length, 1)),
+                         ps=unlist(map)*1e6,
+                         rs=unlist(lapply(map, names)),
+                         p_wald = 10^(-lod),
+                         LOD = lod,
+                         stringsAsFactors=FALSE)
+    rownames(result) <- NULL
+
     # write qtl2 data
     outfile <- file.path(dir, "data", "gwas.json")
+    cat(jsonlite::toJSON(result), file=outfile)
 
     browseURL(paste0("file://", file.path(dir, "index.html")))
+
+    invisible(dir)
 }
